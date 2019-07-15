1b0a78c9fad1928ee819333c06f3b617e3d67259
scauzml <13828607279@163.com>
2019-07-15 10:45:03 +0800

    should_return_expense_type_A_if_project_is_external_and_name_is_project_A

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -26,8 +26,14 @@ class ExpenseServiceTest {
     @Test
     void should_return_expense_type_A_if_project_is_external_and_name_is_project_A() throws UnexpectedProjectTypeException {
         // given
+        Project project = new Project(ProjectType.EXTERNAL,"Project A");
+
         // when
+        ExpenseType expenseCodeByProjectTypeAndName = ExpenseService.getExpenseCodeByProjectTypeAndName(project);
+
         // then
+        Assertions.assertEquals(ExpenseType.EXPENSE_TYPE_A, expenseCodeByProjectTypeAndName);
+
     }
 
     @Test
